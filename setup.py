8a3d768e415602945926c5e4208406e180a80bb6
Calvin Spealman <calvin@pantechnoco.com>
2012-05-17 04:57:39 -0400

    Add straight.plugin to install_requires

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ setup(name='straight.command',
     author_email='ironfroggy@gmail.com',
     url='https://github.com/ironfroggy/straight.plugin',
     packages=['straight', 'straight.command'],
+    install_requires=[
+        'straight.plugin',
+    ],
     classifiers=[
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
